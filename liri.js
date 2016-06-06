8ba45462530e3d68df6023ddc43f9c8db673b0d6
Joshua Cruey <Joshua@crueyphotography.com>
2016-06-05 20:26:22 -0400

    working with twitter response and seeing tweets

--- liri.js
+++ liri.js
@@ -4,9 +4,11 @@ var keys = require('./keys.js')
 
 var twitter = new Twitter(keys.twitterKeys);
  
-var params = {screen_name: 'jccruey'};
+var params = {screen_name: 'jccruey', count: 20};
 twitter.get('statuses/user_timeline', params, function(error, tweets, response){
   if (!error) {
-    console.log(response.text);
+  	for (var i = 0; i < tweets.length; i++) {
+  		console.log(tweets[i].created_at + " " + tweets[i].text)
+  	}
   }
 });
