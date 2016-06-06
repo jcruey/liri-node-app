f0be2f5620a56704a0524d22219bc32ce3bb0c1d
Joshua Cruey <Joshua@crueyphotography.com>
2016-06-06 10:46:18 -0400

    organized various api requests into function and added switch functionality to accept argurments

--- liri.js
+++ liri.js
@@ -1,22 +1,53 @@
+//load the file system module
+var fs = require('fs');
+//load the twitter module
 var Twitter = require('twitter');
- 
+ //load the Auth keys for Twitter
 var keys = require('./keys.js')
-
+//load the Spotify module
 var spotify = require('spotify');
+//load the request module
+var request = require('request');
 
-// var twitter = new Twitter(keys.twitterKeys);
+
+// Take two arguments 
+var action = process.argv[2];
+var value = process.argv[3];
+
+// We will then create a switch-case statement (if-then would also work).
+// The switch-case will direct which function gets run.
+switch(action){
+    case 'my-tweets':
+        getTweets();
+        break;
+    case 'spotify-this-song':
+        searchSpotify();
+        break;
+    case 'movie-this':
+        searchMovie();
+        break;
+    case 'do-what-it-says':
+        cmdFromTxt();
+        break;
+}
  
-// var params = {screen_name: 'jccruey', count: 20};
-// twitter.get('statuses/user_timeline', params, function(error, tweets, response){
-//   if (!error) {
-//   	for (var i = 0; i < tweets.length; i++) {
-//   		console.log(tweets[i].created_at + " " + tweets[i].text)
-//   	}
-//   }
-// });
+function getTweets() { 
+//Create a new Twitter instance
+var twitter = new Twitter(keys.twitterKeys);
+twitter.get('statuses/user_timeline', {screen_name: 'jccruey', count: 20}, function(err, tweets, response){
+  if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    } else {
+  	for (var i = 0; i < tweets.length; i++) {
+  		console.log(tweets[i].created_at + " " + tweets[i].text)
+  	}
+  }
+});
+}
 
-query = process.argv[2]; 
-spotify.search({ type: 'track', query: query || "what's my age again" }, function(err, data) {
+function searchSpotify() {
+spotify.search({ type: 'track', query: value || "what's my age again" }, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
@@ -26,6 +57,30 @@ spotify.search({ type: 'track', query: query || "what's my age again" }, functio
         console.log('Preview Link: ' + data.tracks.items[0].preview_url);
         console.log('Album: ' + data.tracks.items[0].album.name);
     }
- 	
-   
-});
+ });
+}
+
+function searchMovie() {
+	var queryUrl = 'http://www.omdbapi.com/?t=' + value || "Mr. Nobody" +'&y=&plot=short&r=json&tomatoes=true';
+	request(queryUrl, function(err, response, body) {
+	if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    } else {
+		body = JSON.parse(body);
+		console.log(body);
+		console.log("Title: " + body.Title);
+		console.log("Year: " + body.Year);
+		console.log("IMDB Rating: " + body.imdbRating);
+		console.log("Country: " + body.Country);
+		console.log("Language: " + body.Language);
+		console.log("Plot: " + body.Plot);
+		console.log("Actors :" + body.Actors);
+		console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+		console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+	}
+})
+}
+
+
